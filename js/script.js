19b98715d428deb97a9f3708052ef68d1e131963
matthewobell <matt.o.bell@gmail.com>
2026-04-17 17:07:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,44 @@ if (toggle && nav) {
   });
 }
 
+function updateCarouselButtons(carousel) {
+  const wrapper = carousel.closest(".carousel-wrapper");
+  if (!wrapper) return;
+
+  const leftButton = wrapper.querySelector('.carousel-button[data-direction="left"]');
+  const rightButton = wrapper.querySelector('.carousel-button[data-direction="right"]');
+
+  if (!leftButton || !rightButton) return;
+
+  const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+  const currentScroll = carousel.scrollLeft;
+
+  if (currentScroll <= 8) {
+    leftButton.classList.add("is-hidden");
+  } else {
+    leftButton.classList.remove("is-hidden");
+  }
+
+  if (currentScroll >= maxScrollLeft - 8) {
+    rightButton.classList.add("is-hidden");
+  } else {
+    rightButton.classList.remove("is-hidden");
+  }
+}
+
+const carousels = document.querySelectorAll(".event-carousel");
+carousels.forEach((carousel) => {
+  updateCarouselButtons(carousel);
+
+  carousel.addEventListener("scroll", () => {
+    updateCarouselButtons(carousel);
+  });
+
+  window.addEventListener("resize", () => {
+    updateCarouselButtons(carousel);
+  });
+});
+
 const carouselButtons = document.querySelectorAll(".carousel-button");
 
 carouselButtons.forEach((button) => {
@@ -23,5 +61,9 @@ carouselButtons.forEach((button) => {
       left: direction === "right" ? scrollAmount : -scrollAmount,
       behavior: "smooth"
     });
+
+    setTimeout(() => {
+      updateCarouselButtons(carousel);
+    }, 350);
   });
 });
